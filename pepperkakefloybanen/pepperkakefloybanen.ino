fc83e7cf5d8137da63df7e220cbf7828f4b4bcb7
Bård Aase <elzapp@gmail.com>
2015-11-15 22:45:49 +0100

    Den versjonen som nå kjører i pepperkakebyen

--- pepperkakefloybanen/pepperkakefloybanen.ino
+++ pepperkakefloybanen/pepperkakefloybanen.ino
@@ -8,10 +8,19 @@ Servo myservo;  // create servo object to control a servo
 Servo myservo2;  // create servo object to control a servo
                 // twelve servo objects can be created on most boards
 
+
+// Editable parameters
+// How many seconds should it go downwards (upwards movement controlled by sensor)
+int goDownTime=600;
+
+// How long break at the end station (milliseconds)?
+const int motorstoptime = 10*60000; // hvor lang pause før retning snus
+
+
+
 int pos = 0;    // variable to store the servo position
 int led = 13;
 int motorruntime = 0; // hvor lenge motor skal gå i en retning
-const int motorstoptime = 5000; // hvor lang pause før retning snus
 int motorspeed = 0;
 boolean direction = 0; // 0 = opp, 1 = ned
 
@@ -20,6 +29,22 @@ int upArr[] = {79, 80, 81, 82, 83, 84, 85};
 int downArr[] = {104, 103, 102, 101, 100, 99, 98};
 //int downArr[] = {100, 105, 110, 115, 120, 125, 130};
 
+int topSensorBM=5;
+int topSensorRH=6;
+
+int downSpeedRH=99;
+int downSpeedBM=84;
+
+int upSpeedRH=83;
+int upSpeedBM=98;
+
+int topReachedRH=0;
+int topReachedBM=0;
+
+
+int topStatusRH=0;
+int topStatusBM=0;
+
 void setup()
 {
   //delay(10000);
@@ -27,50 +52,66 @@ void setup()
   myservo2.attach(3);  // attaches the servo on pin 9 to the servo object
   Serial.begin(9600);
   pinMode(led, OUTPUT);
-  myservo.write(90);
-  myservo2.write(90);
+  //myservo.write(90);
+  //myservo2.write(90);
+  myservo.write(upSpeedRH);
+  myservo2.write(upSpeedBM); //Blaamann
 }
+
+boolean sync(){
+  if(!topReachedBM)topReachedBM=topStatusBM;
+  else{
+    myservo2.write(90);
+    //Serial.println("Blaamann er oppe!");
+  }
+  if(!topReachedRH)topReachedRH=topStatusRH;
+  else{
+    myservo.write(90);
+    //Serial.println("Roedhette er oppe!");
+  }
+  if(topReachedBM && topReachedRH){
+    return 1;
+  }
+  return 0;
+}
+
+
 
 void loop()
 {
-  int sensorValueA4 = analogRead(A4);
-  int sensorValueA5 = analogRead(A5);
-  int runtimeval = map(sensorValueA4, 0, 1023, 10, 40);
-  int speedval = map(sensorValueA5, 0, 1023, 0, 6); // OBSOBS - størrelse på upArr/downArr må være lik
-
-  Serial.print(sensorValueA4);
-  Serial.print(" -> ");
-  Serial.print(runtimeval);
-  Serial.print(" - ");
-  Serial.print(sensorValueA5);
-  Serial.print(" -> ");
-  Serial.println(speedval);
-
+  topStatusBM=digitalRead(topSensorBM);
+  topStatusRH=digitalRead(topSensorRH);
+  Serial.print(topStatusBM);
+  Serial.print(topStatusRH);
+  Serial.print("\r");
+  if(sync()){
+  int runtimeval = goDownTime;//map(sensorValueA4, 0, 1023, 10, 40);
   digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
 
-  if (direction)
+  int rh=90;
+  int bm=90;
+  if (direction) // Blamann goes UP
   {
-    Serial.print(motorruntime);
-    Serial.print(" -> ");
-    Serial.print(upArr[speedval]);
-    Serial.println(" - up");
-    myservo.write(upArr[speedval]);
-    myservo2.write(upArr[speedval]);
+    if(motorruntime <= runtimeval)rh=downSpeedRH;
+    if(!topStatusBM)bm=upSpeedBM;
   } else
-  {
-    Serial.print(motorruntime);
-    Serial.print(" -> ");
-    Serial.print(downArr[speedval]);
-    Serial.println(" - down");
-    myservo.write(downArr[speedval]);
-    myservo2.write(downArr[speedval]);
+  { //Blamann goes DOWN
+    if(motorruntime <= runtimeval)bm=downSpeedBM;
+    if(!topStatusRH)rh=upSpeedRH;
   }
+  Serial.print(motorruntime);
+  Serial.print(" Rodhette: ");
+  Serial.print(rh);
+  Serial.print(" Blaamann: ");
+  Serial.print(bm);
+  myservo.write(rh);
+  myservo2.write(bm);
 
 
   digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
   delay(1000);
   motorruntime += 1;
-  if (motorruntime > runtimeval) {
+  if (motorruntime > runtimeval && (topStatusBM||topStatusRH)) {
     Serial.println("Delay and stop before reversing");
     motorruntime = 0;
     // Stopper servoer/motorer
@@ -81,5 +122,6 @@ void loop()
     if (direction) {
       direction = 0;
     } else { direction = 1; }
+  }
   }
 }
