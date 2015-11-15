a338748dedcd9bebb2ee693dfed0f93628b35f4d
Roger Storvik <rogers@pc3044.pc.met.no>
2015-11-15 17:58:35 +0100

    Lagt inn forslag til init fra Huy, med noen syntaxtilpasninger og sjekk om sensorer allerede er aktive.

--- ParallaxFloyen.ino
+++ ParallaxFloyen.ino
@@ -8,12 +8,21 @@ Servo myservo;  // create servo object to control a servo
 Servo myservo2;  // create servo object to control a servo 
                 // twelve servo objects can be created on most boards
  
+ 
+const byte EndSensorA = 5;
+const byte EndSensorB = 6;
+boolean EndSensorATrigger = false;
+boolean EndSensorBTrigger = false;
+boolean EndSensorA_active = false;
+boolean EndSensorB_active = false;
+
+
 int pos = 0;    // variable to store the servo position 
-int led = 13;
+const byte led = 13;
 int motorruntime = 0; // hvor lenge motor skal gå i en retning 
 const int motorstoptime = 5000; // hvor lang pause før retning snus
 int motorspeed = 0;
-boolean direction = 0; // 0 = opp, 1 = ned
+boolean direction = false; // false = opp, true = ned
 
 int upArr[] = {79, 80, 81, 82, 83, 84, 85};
 int downArr[] = {104, 103, 102, 101, 100, 99, 98};
@@ -21,12 +30,71 @@ int downArr[] = {104, 103, 102, 101, 100, 99, 98};
 void setup() 
 { 
   //delay(10000);
-  myservo.attach(2);  // attaches the servo on pin 9 to the servo object 
-  myservo2.attach(3);  // attaches the servo on pin 9 to the servo object 
+  myservo.attach(2);  // attaches the servo on pin 2 to the servo object 
+  myservo2.attach(3);  // attaches the servo on pin 3 to the servo object 
   Serial.begin(9600);
   pinMode(led, OUTPUT);
+  pinMode(EndSensorA, INPUT);
+  pinMode(EndSensorB, INPUT);
   myservo.write(90);
-  myservo2.write(90);
+  myservo2.write(90); //stop motors
+  
+  //init both motors
+  //both motors need to go into the up direction to hit the switches
+
+  // Check if endsensors are already active before resetting motors
+  if (digitalRead(EndSensorA)) 
+  {
+    EndSensorA_active = true;
+  }
+  if (digitalRead(EndSensorB)) 
+  {
+    EndSensorB_active = true;
+  }
+  
+  if (!EndSensorA_active) 
+  {
+    myservo.write(85);
+  }
+  if (!EndSensorB_active) {
+    myservo2.write(85);
+  }
+  
+  while (true)
+  {
+      if (digitalRead(EndSensorA))
+      {
+         myservo.write(90);
+         EndSensorATrigger = true;
+      }
+      
+      if (digitalRead(EndSensorB))
+      {
+         myservo2.write(90);
+         EndSensorBTrigger = true;
+      }
+      
+      if (EndSensorATrigger && EndSensorBTrigger)
+      {
+        
+        //when motors hit the sensors, go into the reverse direction for 2 seconds (same direction)
+        myservo.write(180);
+        myservo2.write(0);
+        delay(2000);
+        
+        //when 2 seconds has gone, stop motor 1, but keep it going for motor 2 for the next
+        //40 seconds
+        myservo.write(90);
+        myservo2.write(0);
+        delay(40000);
+        //once motor 2 has gone 40 seconds, stop motor 2
+        myservo2.write(90);
+        break; 
+      }
+      // wait until both triggers have been 
+  }
+  //motors are now initialised
+  
 } 
  
 void loop() 
@@ -54,7 +122,8 @@ void loop()
     Serial.println(" - up");
     myservo.write(upArr[speedval]);
     myservo2.write(upArr[speedval]);
-  } else 
+  } 
+  else 
   {
     Serial.print(motorruntime);
     Serial.print(" -> ");
@@ -62,11 +131,9 @@ void loop()
     Serial.println(" - down");
     myservo.write(downArr[speedval]);
     myservo2.write(downArr[speedval]);
-  }  
- 
- 
+  }   
   digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);
+  delay(1000); //if using sensors to reset it each time it goes up, then this cant //be here..
   motorruntime += 1;
   if (motorruntime > runtimeval) {
     Serial.println("Delay and stop before reversing");
@@ -77,8 +144,7 @@ void loop()
     delay(motorstoptime); // Pause hvis oppe eller nede
     // skifte retning
     if (direction) { 
-      direction = 0; 
-    } else { direction = 1; }
+      direction = false; 
+    } else { direction = true; }
   }
 } 
-
