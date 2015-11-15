1315634dfc28942c4b2b7dfe6dbbcd1188eaa0d5
Roger Storvik <roger.storvik@gmail.com>
2015-11-15 22:36:46 +0100

    Endringer for å øke lesbarhet..

--- ParallaxFloyen.ino
+++ ParallaxFloyen.ino
@@ -9,8 +9,8 @@ Servo myservo2;  // create servo object to control a servo
                 // twelve servo objects can be created on most boards
  
  
-const byte EndSensorA = 5;
-const byte EndSensorB = 6;
+const byte EndSensorA = 5; // venstre dvs rød lue
+const byte EndSensorB = 6; // høyre dvs blå lue
 boolean EndSensorATrigger = false;
 boolean EndSensorBTrigger = false;
 boolean EndSensorA_active = false;
@@ -27,22 +27,21 @@ boolean direction = false; // false = opp, true = ned
 int upArr[] = {79, 80, 81, 82, 83, 84, 85};
 int downArr[] = {104, 103, 102, 101, 100, 99, 98};
 
-const int upMotor = 85;
-const int downMotor = 98;
+const int upMotor = 84; // 136s per finnelengde
+const int downMotor = 105; // 
 const int stopMotor = 90;
+const int runtimeval = 1200; // * 1000ms per session
 
 void setup() 
 { 
   //delay(10000);
-  myservo.attach(2);  // attaches the servo on pin 2 to the servo object 
-  myservo2.attach(3);  // attaches the servo on pin 3 to the servo object 
+  myservo.attach(2);  // Venstre vogn/motor dvs rød lue
+  myservo2.attach(3);  // Høyre vogn/motor dvs blå lue
   Serial.begin(9600);
   pinMode(led, OUTPUT);
   pinMode(EndSensorA, INPUT);
   pinMode(EndSensorB, INPUT);
-  myservo.write(stopMotor);
-  myservo2.write(stopMotor); //stop motors
-  
+
   //init both motors
   //both motors need to go into the up direction to hit the switches
 
@@ -83,14 +82,14 @@ void setup()
         
         //when motors hit the sensors, go into the reverse direction for 2 seconds (same direction)
         myservo.write(downMotor);
-        myservo2.write(upMotor);
+        myservo2.write(downMotor);
         delay(2000);
         
         //when 2 seconds has gone, stop motor 1, but keep it going for motor 2 for the next
         //40 seconds
         myservo.write(stopMotor);
-        myservo2.write(upMotor);
-        delay(40000);
+        myservo2.write(downMotor);
+        delay(120000);
         //once motor 2 has gone 40 seconds, stop motor 2
         myservo2.write(stopMotor);
         break; 
@@ -98,43 +97,26 @@ void setup()
       // wait until both triggers have been 
   }
   //motors are now initialised
-  
 } 
- 
+
 void loop() 
 { 
   int sensorValueA4 = analogRead(A4);
-  int sensorValueA5 = analogRead(A5);
-  int runtimeval = map(sensorValueA4, 0, 1023, 10, 40);
-  int speedval = map(sensorValueA5, 0, 1023, 0, 6); // OBSOBS - størrelse på upArr/downArr må være lik
-  
-  Serial.print(sensorValueA4);
-  Serial.print(" -> ");
-  Serial.print(runtimeval);
-  Serial.print(" - ");
-  Serial.print(sensorValueA5);
-  Serial.print(" -> ");
-  Serial.println(speedval);
+  int sensorValueA5 = analogRead(A5);  
 
   digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
 
   if (direction) 
   {
-    Serial.print(motorruntime);
-    Serial.print(" -> ");
-    Serial.print(upArr[speedval]);
-    Serial.println(" - up");
-    myservo.write(upArr[speedval]);
-    myservo2.write(upArr[speedval]);
+    Serial.println("up");
+    myservo.write(downMotor);
+    myservo2.write(upMotor);
   } 
   else 
   {
-    Serial.print(motorruntime);
-    Serial.print(" -> ");
-    Serial.print(downArr[speedval]);
-    Serial.println(" - down");
-    myservo.write(downArr[speedval]);
-    myservo2.write(downArr[speedval]);
+    Serial.println("down");
+    myservo.write(upMotor);
+    myservo2.write(downMotor);
   }   
   digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
   delay(1000); //if using sensors to reset it each time it goes up, then this cant //be here..
@@ -143,8 +125,8 @@ void loop()
     Serial.println("Delay and stop before reversing");
     motorruntime = 0;
     // Stopper servoer/motorer
-    myservo.write(90);
-    myservo2.write(90);
+    myservo.write(stopMotor);
+    myservo2.write(stopMotor);
     delay(motorstoptime); // Pause hvis oppe eller nede
     // skifte retning
     if (direction) { 
